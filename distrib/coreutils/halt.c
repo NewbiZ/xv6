a478dc823445f833f2cc59e609084a56795572a1
newbiz <vallee.aurelien@gmail.com>
2014-08-09 01:10:43 +0300

    halt will now print a message before shutdown

--- distrib/coreutils/halt.c
+++ distrib/coreutils/halt.c
@@ -4,8 +4,11 @@
 int
 main(int argc, char *argv[])
 {
+  __ulibc_printf(1, "The system is going down for halt NOW\n");
+
   if (halt() < 0)
     __ulibc_printf(2, "failed to halt");
+
   sysexit();
 }
 
